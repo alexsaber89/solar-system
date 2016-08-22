0a01a1a087b790ae53be1e878ac86d695c88d2c7
Alex Saber <Alex Saber>
2016-08-21 20:29:28 -0500

    utilized array.reduce to form a sentence from the words array

--- main.js
+++ main.js
@@ -9,26 +9,29 @@ var el = document.getElementById("planets");
 function logArrayElements(element) {
   el.innerHTML += '<p>' + element + '</p>';
 }
-
 planets.forEach(logArrayElements);
 console.log(planets);
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
-var capPlanets = planets.map(capitalizeFirstLetter)
 function capitalizeFirstLetter(arrayElement) {
   return arrayElement.charAt(0).toUpperCase() + arrayElement.slice(1);
 }
+var capPlanets = planets.map(capitalizeFirstLetter)
 console.log(capPlanets);
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
-var planetsOnlyE = planets.filter(onlyE);
 function onlyE(arrayElement) {
   if(arrayElement.indexOf('e') != -1) {
     return arrayElement;
   }
 }
+var planetsOnlyE = planets.filter(onlyE);
 console.log(planetsOnlyE);
 
 // Use the reduce method to create a sentence from the words in the following array
 
 var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+function arraySentence(pv,cv) {
+  return pv += " " + cv;
+}
+console.log(words.reduce(arraySentence));
